1cf00c21e9d556ef7cc32aca59c28ab20e97016b
Maria Vicente <mnvcaetano@gmail.com>
2023-11-23 11:56:48 -0300

    cria API de produto

---
+++ src/main/java/caetano/maria/springboot/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package caetano.maria.springboot.repositories;
+
+import caetano.maria.springboot.models.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+}
